70944791aa7fce0a40c6308898d9804a1f4dd5df
Alex Fiannaca <t-alfian@microsoft.com>
2015-08-31 12:48:02 -0700

    Adding windows support

---
+++ merges/windows/scripts/platformOverrides.js
@@ -0,0 +1,41 @@
+﻿if (!SpeechSynthesisUtterance) {
+    function SpeechSynthesisUtterance(txt) {
+        return {
+            text: txt
+        };
+    };
+}
+
+(function () {
+    // Append the safeHTML polyfill
+    var scriptElem = document.createElement('script');
+    scriptElem.setAttribute('src', 'scripts/winstore-jscompat.js');
+    if (document.body) {
+        document.body.appendChild(scriptElem);
+    } else {
+        document.head.appendChild(scriptElem);
+    }
+
+    // Use the Windows specific code for generating speech
+    if (!window.speechSynthesis) {
+        window.speechSynthesis = {
+            speak: function (utterance) {
+                var audio = new Audio();
+                var synth = Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+
+                synth.synthesizeTextToStreamAsync(utterance.text).then(function (markersStream) {
+                    // Convert the stream to a URL Blob.
+                    var blob = MSApp.createBlobFromRandomAccessStream(markersStream.ContentType, markersStream);
+
+                    // Send the Blob to the audio object.
+                    audio.src = URL.createObjectURL(blob, { oneTimeOnly: true });
+                    audio.onended = function () {
+                        console.log("Done speaking... Use a promise here...");
+                    };
+                    audio.play();
+                });
+            }
+        };
+    }
+
+}());
